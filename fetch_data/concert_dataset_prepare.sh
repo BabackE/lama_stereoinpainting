d5bfeb31bc141514893e34430ffa46fa6bab3b00
Baback <baback@cs.washington.edu>
2024-06-17 18:08:44 -0700

    recover 1080p v

--- fetch_data/concert_dataset_prepare.sh
+++ fetch_data/concert_dataset_prepare.sh
@@ -1,26 +1,26 @@
 # Split: split train -> train & val
 echo creating random set
-cat concert/fetch_lists/train_shuffled.flist | shuf > concert/train256/temp_train_shuffled.flist
-cat concert/train256/temp_train_shuffled.flist | head -n 2000 > concert/train256/val_shuffled.flist
-cat concert/train256/temp_train_shuffled.flist | tail -n +2001 > concert/train256/train_shuffled.flist
-cat concert/fetch_lists/val_shuffled.flist > concert/train256/visual_test_shuffled.flist
+cat concert/fetch_lists/train_shuffled.flist | shuf > concert/train/temp_train_shuffled.flist
+cat concert/train/temp_train_shuffled.flist | head -n 2000 > concert/train/val_shuffled.flist
+cat concert/train/temp_train_shuffled.flist | tail -n +2001 > concert/train/train_shuffled.flist
+cat concert/fetch_lists/val_shuffled.flist > concert/train/visual_test_shuffled.flist
 
-mkdir concert/train256/train_256/
-mkdir concert/train256/val_source_256/
-mkdir concert/train256/visual_test_source_256/
+mkdir concert/train/train_256/
+mkdir concert/train/val_source_256/
+mkdir concert/train/visual_test_source_256/
 
 echo moving images into assigned buckets
-cat concert/train256/train_shuffled.flist | xargs -I {} mv concert/train256/256/{} concert/train256/train_256/
-cat concert/train256/val_shuffled.flist | xargs -I {} mv concert/train256/256/{} concert/train256/val_source_256/
-cat concert/train256/visual_test_shuffled.flist | xargs -I {} mv concert/train256/256/{} concert/train256/visual_test_source_256/
+cat concert/train/train_shuffled.flist | xargs -I {} mv concert/train/1080p/{} concert/train/train_256/
+cat concert/train/val_shuffled.flist | xargs -I {} mv concert/train/1080p/{} concert/train/val_source_256/
+cat concert/train/visual_test_shuffled.flist | xargs -I {} mv concert/train/1080p/{} concert/train/visual_test_source_256/
 
 echo creating training scripts
 
 # create location config concert.yaml
 PWD=$(pwd)
-DATASET=${PWD}/concert/train256
-CONCERT_DST=${PWD}/configs/training/location/concert256.yaml
-CONCERT_SRC=${PWD}/concert/concert256.yaml
+DATASET=${PWD}/concert/train
+CONCERT_DST=${PWD}/configs/training/location/concert.yaml
+CONCERT_SRC=${PWD}/concert/concert.yaml
 
 touch $CONCERT_SRC
 echo "# @package _group_" >> $CONCERT_SRC
