86a761a35fe7a7a7e1140c2f4303f15ca463fa26
Baback <baback@cs.washington.edu>
2024-06-20 15:27:58 -0700

    path fixes

--- fetch_data/concert_dataset_prepare_720p_rand.sh
+++ fetch_data/concert_dataset_prepare_720p_rand.sh
@@ -3,27 +3,27 @@ DATASET_SUFFIX=720
 
 # Split: split train -> train & val
 echo creating random set
-cat {$TRAINING_ROOT}/fetch_lists/train_shuffled.flist | shuf > {$TRAINING_ROOT}/temp_train_shuffled.flist
-cat {$TRAINING_ROOT}/temp_train_shuffled.flist | head -n 2000 > {$TRAINING_ROOT}/val_shuffled.flist
-cat {$TRAINING_ROOT}/temp_train_shuffled.flist | tail -n +2001 > {$TRAINING_ROOT}/train_shuffled.flist
-cat {$TRAINING_ROOT}/fetch_lists/val_shuffled.flist > {$TRAINING_ROOT}/visual_test_shuffled.flist
+cat $TRAINING_ROOT/fetch_lists/train_shuffled.flist | shuf > $TRAINING_ROOT/temp_train_shuffled.flist
+cat $TRAINING_ROOT/temp_train_shuffled.flist | head -n 2000 > $TRAINING_ROOT/val_shuffled.flist
+cat $TRAINING_ROOT/temp_train_shuffled.flist | tail -n +2001 > $TRAINING_ROOT/train_shuffled.flist
+cat $TRAINING_ROOT/fetch_lists/val_shuffled.flist > $TRAINING_ROOT/visual_test_shuffled.flist
 
-mkdir {$TRAINING_ROOT}/train_{$DATASET_SUFFIX}/
-mkdir {$TRAINING_ROOT}/val_source_${DATASET_SUFFIX}/
-mkdir {$TRAINING_ROOT}/visual_test_source_${DATASET_SUFFIX}/
+mkdir $TRAINING_ROOT/train_$DATASET_SUFFIX/
+mkdir $TRAINING_ROOT/val_source_$DATASET_SUFFIX/
+mkdir $TRAINING_ROOT/visual_test_source_$DATASET_SUFFIX/
 
 echo moving images into assigned buckets
-cat {$TRAINING_ROOT}/train_shuffled.flist | xargs -I {} mv {$TRAINING_ROOT}/{$DATASET_SUFFIX}/{} {$TRAINING_ROOT}/train_${DATASET_SUFFIX}/
-cat {$TRAINING_ROOT}/val_shuffled.flist | xargs -I {} mv {$TRAINING_ROOT}/${DATASET_SUFFIX}/{} {$TRAINING_ROOT}/val_source_${DATASET_SUFFIX}/
-cat {$TRAINING_ROOT}/visual_test_shuffled.flist | xargs -I {} mv {$TRAINING_ROOT}/${DATASET_SUFFIX}/{} {$TRAINING_ROOT}/visual_test_source_${DATASET_SUFFIX}/
+cat $TRAINING_ROOT/train_shuffled.flist | xargs -I {} mv $TRAINING_ROOT/{$DATASET_SUFFIX}/{} $TRAINING_ROOT/train_$DATASET_SUFFIX/
+cat $TRAINING_ROOT/val_shuffled.flist | xargs -I {} mv $TRAINING_ROOT/$DATASET_SUFFIX/{} $TRAINING_ROOT/val_source_$DATASET_SUFFIX/
+cat $TRAINING_ROOT/visual_test_shuffled.flist | xargs -I {} mv $TRAINING_ROOT/$DATASET_SUFFIX/{} $TRAINING_ROOT/visual_test_source_$DATASET_SUFFIX/
 
 echo creating training scripts
 
 # create location config concert.yaml
 PWD=$(pwd)
-DATASET=${PWD}/{$TRAINING_ROOT}
-CONCERT_DST=${PWD}/configs/training/location/concert_${DATASET_SUFFIX}_rand.yaml
-CONCERT_SRC=${PWD}/concert/concert_${DATASET_SUFFIX}_rand.yaml
+DATASET=${PWD}/$TRAINING_ROOT
+CONCERT_DST=${PWD}/configs/training/location/concert_$DATASET_SUFFIX_rand.yaml
+CONCERT_SRC=${PWD}/concert/concert_$DATASET_SUFFIX_rand.yaml
 
 touch $CONCERT_SRC
 echo "# @package _group_" >> $CONCERT_SRC
