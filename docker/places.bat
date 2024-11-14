09869cf54a01664ff1c106d8f475e2eda00c142f
BabackE <belmieh@gmail.com>
2024-11-13 22:26:10 -0800

    progress on places training setup

---
+++ docker/places.bat
@@ -0,0 +1,14 @@
+SET CURDIR=%CD%
+SET SRCDIR=%CD%\..
+
+docker run --gpus all -it --rm --shm-size=8gb --env="DISPLAY" ^
+--volume="%SRCDIR%\ade20k":/home/user/lama/ade20k ^
+--volume="%SRCDIR%\celeba-hq-dataset":/home/user/lama/celeba-hq-dataset ^
+--volume="D:\experiments\stereo\e113-lama-training-datasets\se03_bono_720p_stereo_mask_training\720_rand_dataset\concert":/home/user/lama/concert ^
+--volume="%SRCDIR%\experiments":/home/user/lama/experiments ^
+--volume="%SRCDIR%\docker\places\places_standard_dataset":/home/user/lama/places_standard_dataset ^
+--volume="%SRCDIR%\outputs":/home/user/lama/outputs ^
+--volume="%SRCDIR%\models\models":/home/user/lama/models/models ^
+--volume="%SRCDIR%\docker\.ssh\id_ed25119":/home/user/.ssh/id_ed25119 ^
+--volume="%SRCDIR%\hub":/home/user/lama/hub ^
+--name="lama" lama /bin/bash
