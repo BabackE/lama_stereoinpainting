80bf35d6617a793ee50cef4244da8236a3776b01
BabackE <belmieh@gmail.com>
2024-11-17 16:03:52 -0800

    try symlink

--- fetch_data/patch_places_dataset.sh
+++ fetch_data/patch_places_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo rm /home/user/places_standard_dataset/train_large_places365standard_disparity_u8.hdf5
 sudo rm /home/user/places_standard_dataset/val_large_places365standard_disparity_u8.hdf5
-ln /home/user/places_standard_dataset/train_large_places365standard_disparity_u8.hdf5 /home/user/places_standard_dataset/depth/train_large_places365standard_disparity_u8.hdf5 
-ln /home/user/places_standard_dataset/val_large_places365standard_disparity_u8.hdf5 /home/user/places_standard_dataset/depth/val_large_places365standard_disparity_u8.hdf5 
+sudo ln -s /home/user/places_standard_dataset/depth/train_large_places365standard_disparity_u8.hdf5 /home/user/places_standard_dataset/train_large_places365standard_disparity_u8.hdf5 
+sudo ln -s /home/user/places_standard_dataset/depth/val_large_places365standard_disparity_u8.hdf5 /home/user/places_standard_dataset/val_large_places365standard_disparity_u8.hdf5 
