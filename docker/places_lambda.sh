d3636927cfa68b16e8e7a33f420ee7729db6d288
BabackE <belmieh@gmail.com>
2024-11-26 22:25:14 -0800

    path fix

--- docker/places_lambda.sh
+++ docker/places_lambda.sh
@@ -11,6 +11,6 @@ sudo docker run --gpus all -it --rm --shm-size=8gb --env="DISPLAY" \
 --volume="${SRCDIR}/../outputs:/home/user/lama/outputs" \
 --volume="${SRCDIR}/../outputs/tb_logs:/home/user/lama/tb_logs" \
 --volume="${SRCDIR}/../places/depth:/home/user/lama/places_depth" \
---volume="${SRCDIR}/../places/depth:/home/user/lama/places_standard_dataset" \
+--volume="${SRCDIR}/../places:/home/user/lama/places_standard_dataset" \
 --volume="${SRCDIR}/docker/.ssh:/home/user/.ssh" \
 --name="lama" lama /bin/bash
