3dcfc4f1ded958eb2aec3c0d3654c431f69288f1
BabackE <belmieh@gmail.com>
2024-11-26 22:21:10 -0800

    add dataset

--- docker/places_lambda.sh
+++ docker/places_lambda.sh
@@ -11,5 +11,6 @@ docker run --gpus all -it --rm --shm-size=8gb --env="DISPLAY" \
 --volume="${SRCDIR}/../outputs:/home/user/lama/outputs" \
 --volume="${SRCDIR}/../outputs/tb_logs:/home/user/lama/tb_logs" \
 --volume="${SRCDIR}/../places/depth:/home/user/lama/places_depth" \
+--volume="${SRCDIR}/../places/depth:/home/user/lama/places_standard_dataset" \
 --volume="${SRCDIR}/docker/.ssh:/home/user/.ssh" \
 --name="lama" lama /bin/bash
